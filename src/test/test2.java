662bcc4db5eb2381ea6dd36d1e2bb0461358188e
FRANCESCO.BRILLANTE <FRANCESCO.BRILLANTE@LP-PBVVEKX.PROD.HCLPNP.COM>
2018-02-20 15:42:02 +0100

    hello again

--- src/test/test2.java
+++ src/test/test2.java
@@ -1,10 +1,12 @@
 package test;
 
-public class test {	
+public class test2 {	
 	public static String pippo = "sdaasdsd";
 	public static int hello = 1;
 	
-	public test() {
+	public static String helloworld = "hello world";
+	
+	public test2() {
 		// TODO Auto-generated constructor stub
 	}
 }
