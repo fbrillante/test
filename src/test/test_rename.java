a84f36d4dbd85208bc35418ecb93876601e50a99
FRANCESCO.BRILLANTE <FRANCESCO.BRILLANTE@LP-PBVVEKX.PROD.HCLPNP.COM>
2018-02-20 15:56:10 +0100

    ciao vincenzina

--- src/test/test_rename.java
+++ src/test/test_rename.java
@@ -1,10 +1,10 @@
 package test;
 
-public class test {	
+public class test_rename {	
 	public static String pippo = "sdaasdsd";
 	public static int hello = 1;
 	
-	public test() {
+	public test_rename() {
 		// TODO Auto-generated constructor stub
 	}
 }
